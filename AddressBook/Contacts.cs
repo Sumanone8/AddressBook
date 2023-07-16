553c30c1ec015db0f776677c780d4e2439f2cf38
Sumanone8 <sumanjhabhl@gmail.com>
2023-07-16 11:05:24 +0530

    Suman : Add Contacts feature --> Implement Contact class with first and last names, address, city, state, zip, phone number, and email properties.

--- AddressBook/Contacts.cs
+++ AddressBook/Contacts.cs
@@ -1,12 +1,28 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace AddressBook
 {
-    internal class Contacts
+    class Contact
     {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string Zip { get; set; }
+        public string Phone { get; set; }
+        public string Email { get; set; }
+
+        public Contact(string firstName, string lastName, string address, string city, string state, string zip, string phone, string email)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Address = address;
+            City = city;
+            State = state;
+            Zip = zip;
+            Phone = phone;
+            Email = email;
+        }
     }
 }
